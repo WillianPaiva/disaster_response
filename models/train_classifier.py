32c8c3802f1a2bef70607feba50347f4a48aea79
Willian Ver Valem Paiva <vervalenpaiva@gmail.com>
2018-09-15 15:59:33 +0200

    finish the model classifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,69 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+import numpy as np
+import pickle
 
+
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
+    '''
+    loads the sqlite database and returns the features, labels, categories
+    '''
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql("SELECT * FROM disaster_response",engine) 
+
+    X = df['message']
+    # here we drop child allone because we dont have a single message that is classified as such
+    y = df.drop(['id', 'message', 'original', 'genre', 'child_alone'],  axis=1).astype(float)
+    categories = y.columns.values
+    return X, y, categories
 
 
 def tokenize(text):
-    pass
-
+    tokens = nltk.word_tokenize(text)
+    lemmatizer = nltk.WordNetLemmatizer()
+    return [lemmatizer.lemmatize(x).lower().strip() for x in tokens]
 
 def build_model():
-    pass
+    '''
+    builds the pipeline model the parameters where chosen based on the
+    gridsearch that can be checked on the ipynb inside the notebooks folder
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', RandomForestClassifier(max_depth=None,
+                                       min_samples_leaf=1,
+                                       min_samples_split=10,
+                                       n_estimators=20)
+        )
+    ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''return the mode classification report '''
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:  
+        pickle.dump(model, file)
+
 
 
 def main():
@@ -27,13 +72,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
