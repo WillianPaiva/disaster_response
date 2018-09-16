3204787563ca62da50154f85a40ab0c70e22b33f
Willian Ver Valem Paiva <vervalenpaiva@gmail.com>
2018-09-16 13:15:55 +0200

    documentation

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import fbeta_score, make_scorer
+from typing import Tuple, List
 import numpy as np
 import pickle
 
@@ -19,9 +20,16 @@ import pickle
 nltk.download('punkt')
 nltk.download('wordnet')
 
-def load_data(database_filepath):
+def load_data(database_filepath: str)->Tuple[pd.DataFrame, pd.DataFrame, List[str]]:
     '''
-    loads the sqlite database and returns the features, labels, categories
+    loads the sqlite database into pandas dataframes
+    Args:
+        database_filepath: file path of the database to be loaded 
+
+    Returns:
+        X: the messages column (features)
+        y: the categories (one-hot encoded)
+        categories; ordered list of all the categories
     '''
     engine = create_engine('sqlite:///{}'.format(database_filepath))
     df = pd.read_sql("SELECT * FROM disaster_response",engine) 
@@ -32,15 +40,23 @@ def load_data(database_filepath):
     return X, y, categories
 
 
-def tokenize(text):
+def tokenize(text:str)->List[str] :
+    '''
+    tokenizes a given text string
+    Args:
+        text: the string to be tokenized
+    Returns:
+        List; list of tokens (string)
+    '''
     tokens = nltk.word_tokenize(text)
     lemmatizer = nltk.WordNetLemmatizer()
     return [lemmatizer.lemmatize(x).lower().strip() for x in tokens]
 
-def build_model():
+def build_model()->GridSearchCV:
     '''
-    builds the pipeline model the parameters where chosen based on the
-    gridsearch that can be checked on the ipynb inside the notebooks folder
+    Builds the pipelind and the gridsearch 
+    Returns:
+        GridSearchCV: the model 
     '''
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
@@ -69,13 +85,27 @@ def build_model():
     return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    '''return the mode classification report '''
+def evaluate_model(model: GridSearchCV, X_test: pd.DataFrame, Y_test: pd.DataFrame, category_names: List)->None:
+    '''
+    prints the mode classification report
+
+    Args:
+        model: the model to evaluate (GridSearchCV)
+        X_test: the test features to predict
+        Y_test: the TRUE labels to evaluate
+        category_names: a list of each category name
+    '''
     y_pred = model.predict(X_test)
     print(classification_report(Y_test, y_pred, target_names=category_names))
 
 
-def save_model(model, model_filepath):
+def save_model(model: GridSearchCV, model_filepath: str)-> None:
+    '''
+    saves the model on a pickle file
+    Args:
+        model: the model to save
+        model_filepath: the path where to save the pickle file
+    '''
     with open(model_filepath, 'wb') as file:  
         pickle.dump(model, file)
 
